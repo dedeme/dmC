59d3ef1d8a8c921dfec9b51621c08a2b208dc33d
Deme <deme@debian.es>
2019-12-22 13:55:11 +0100

    dmstack: Fixed Iserver

--- app/dmstack/src/primitives/modiserver.c
+++ app/dmstack/src/primitives/modiserver.c
@@ -12,7 +12,6 @@ static void open (Machine *m) {
 }
 
 static void close (Machine *m) {
-
   Iserver *s = (Iserver *)tk_pop_native(m, symbol_ISERVER_);
   iserver_close(s);
 }
@@ -21,15 +20,15 @@ static void getrq (Machine *m) {
   Iserver *s = (Iserver *)tk_pop_native(m, symbol_ISERVER_);
   IserverRq *rq = iserver_read(s);
   char *err = iserverRq_error(rq);
+  Token **values = ATOMIC(sizeof(Token *) + sizeof(Token *));
   if (*err) {
-    machine_push(m, token_new_list(arr_new_from(
-      token_new_string(err), token_new_int(0), NULL
-    )));
+    values[0] = token_new_string(err);
+    values[1] = NULL;
+    machine_push(m, token_from_pointer(symbol_EITHER_, values));
   } else {
-    machine_push(m, token_new_list(arr_new_from(
-      token_from_pointer(symbol_ISERVER_RQ_, rq),
-      NULL
-    )));
+    values[0] = NULL;
+    values[1] = token_from_pointer(symbol_ISERVER_RQ_, rq);
+    machine_push(m, token_from_pointer(symbol_EITHER_, values));
   }
 }
 
@@ -37,11 +36,11 @@ static void readrq (Machine *m) {
   IserverRq *rq = (IserverRq *)tk_pop_native(m, symbol_ISERVER_RQ_);
   char *msg = opt_nget(iserverRq_msg(rq));
   if (msg) {
-    machine_push(m, token_new_list(arr_new_from(
-      token_new_string(msg), NULL
-    )));
+    machine_push(m, token_from_pointer(
+      symbol_OPTION_, token_new_string(msg)
+    ));
   } else {
-    machine_push(m, token_new_list(arr_new()));
+    machine_push(m, token_from_pointer(symbol_OPTION_, NULL));
   }
 }
 
@@ -49,11 +48,11 @@ static void rqhost (Machine *m) {
   IserverRq *rq = (IserverRq *)tk_pop_native(m, symbol_ISERVER_RQ_);
   char *msg = opt_nget(iserverRq_msg(rq));
   if (msg) {
-    machine_push(m, token_new_list(arr_new_from(
-      token_new_string(iserverRq_host(rq)), NULL
-    )));
+    machine_push(m, token_from_pointer(
+      symbol_OPTION_, token_new_string(iserverRq_host(rq))
+    ));
   } else {
-    machine_push(m, token_new_list(arr_new()));
+    machine_push(m, token_from_pointer(symbol_OPTION_, NULL));
   }
 }
 
@@ -71,7 +70,7 @@ Pmodule *modiserver_mk (void) {
 
   add("open", open); // [] - ISERVER
   add("close", close); // ISERVER - []
-  add("getRq", getrq); // ISERVER - OPT<ISERVER_RQ>
+  add("getRq", getrq); // ISERVER - EITHER<STRING, ISERVER_RQ>
   add("readRq", readrq); // ISERVER_RQ - OPT<STRING>
   add("rqHost", rqhost); // ISERVER_RQ - OPT<STRING>
   add("writeRq", writerq); // ISERVER_RQ - []
